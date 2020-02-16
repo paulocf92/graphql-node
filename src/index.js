109bd1816af4e7f5f0fc6f8b7496036fc9976651
Paulo Filho <pcf.feat@gmail.com>
2020-02-16 09:50:49 -0400

    chore: delivering dummy data through compound graphql queries/resolvers

--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-// 1
+// schema definitions
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `;
 
-// 2
+// dummy data
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  },
+  {
+    id: "link-1",
+    url: "www.graphql.org",
+    description: "Official GraphQL Website"
+  }
+];
+
+// resolvers that map query to data retrieval
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links
+  },
+  Link: {
+    id: parent => parent.id,
+    description: parent => parent.description,
+    url: parent => parent.url
   }
 };
 
