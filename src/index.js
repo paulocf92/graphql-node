5281f544469e13205a86bd5540c70fd6bfc6da77
Paulo Filho <pcf.feat@gmail.com>
2020-02-16 12:39:13 -0400

    chore: adding new mutations and refactoring schema

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-// schema definitions
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`;
-
 // dummy data
 let links = [
   {
@@ -27,23 +13,47 @@ let links = [
     description: "Official GraphQL Website"
   }
 ];
+let idCount = links.length;
 
 // resolvers that map query to data retrieval
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links
+    feed: () => links,
+    link: (parent, { id }) => links.find(el => el.id === id)
   },
-  Link: {
-    id: parent => parent.id,
-    description: parent => parent.description,
-    url: parent => parent.url
+  Mutation: {
+    post: (parent, { description, url }) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: description,
+        url: url
+      };
+      links.push(link);
+
+      return link;
+    },
+    updateLink: (parent, { id, url, description }) => {
+      const idx = links.findIndex(el => el.id === id);
+      links[idx].url = url || links[idx].url;
+      links[idx].description = description || links[idx].description;
+
+      return links[idx];
+    },
+    deleteLink: (parent, { id }) => {
+      const idx = links.findIndex(el => el.id === id);
+      const deleted = links[idx];
+
+      links.splice(idx, 1);
+
+      return deleted;
+    }
   }
 };
 
-// 3
+// start up the server passing previously set schema/resolvers
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/schema.graphql",
   resolvers
 });
 
